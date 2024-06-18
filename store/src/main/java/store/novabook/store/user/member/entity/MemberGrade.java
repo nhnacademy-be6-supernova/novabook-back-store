9edd502ec6592153b994d659fed80517e67b7007
ganghagyeong <gkrrod714@naver.com>
2024-06-18 09:55:01 +0900

    #24 feat: admin package 생성

--- store/src/main/java/store/novabook/store/user/member/entity/MemberGrade.java
+++ store/src/main/java/store/novabook/store/user/member/entity/MemberGrade.java
@@ -3,8 +3,13 @@ package store.novabook.store.user.member.entity;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -16,6 +21,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
+@EntityListeners(AuditingEntityListener.class)
 public class MemberGrade {
 
 	@Id
@@ -38,8 +44,10 @@ public class MemberGrade {
 	private BigDecimal discountRate;
 
 	@NotNull
+	@CreatedDate
 	private LocalDateTime createdAt;
 
+	@LastModifiedDate
 	private LocalDateTime updatedAt;
 
 }
--- store/src/main/java/store/novabook/store/user/member/service/MemberService.java
+++ store/src/main/java/store/novabook/store/user/member/service/MemberService.java
@@ -1,11 +1,16 @@
 package store.novabook.store.user.member.service;
 
+import java.time.LocalDateTime;
+
 import org.springframework.stereotype.Service;
 
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import store.novabook.store.user.member.dto.CreateMemberRequest;
+import store.novabook.store.user.member.entity.Member;
+import store.novabook.store.user.member.entity.Users;
 import store.novabook.store.user.member.repository.MemberRepository;
-import store.novabook.store.user.member.repository.UserRepositoy;
+import store.novabook.store.user.member.repository.UsersRepository;
 
 @RequiredArgsConstructor
 @Service
@@ -13,8 +18,31 @@ import store.novabook.store.user.member.repository.UserRepositoy;
 public class MemberService {
 
 	private final MemberRepository memberRepository;
-	private final UserRepositoy userRepositoy;
+	private final UsersRepository usersRepository;
 
+	public Member createMember(CreateMemberRequest createMemberRequest) {
+		Users user = Users.builder()
+			.type(true)
+			.build();
+
+		Member member = Member.builder()
+			.users(user)
+			.loginId(createMemberRequest.loginId())
+			.loginPassword(createMemberRequest.loginPassword())
+			.name(createMemberRequest.name())
+			.number(createMemberRequest.number())
+			.email(createMemberRequest.email())
+			.birth(createMemberRequest.birth())
+			/*.point(5000)*/
+			.latestLoginAt(LocalDateTime.now())
+			.build();
+
+			//TODO: 회원가입시 적립되는 5000포인트가 포인트 내역이 남도록 하는 함수 필요 !!!!!!!
+		return memberRepository.save(member);
+	}
+
+	
 
 
 }
+
