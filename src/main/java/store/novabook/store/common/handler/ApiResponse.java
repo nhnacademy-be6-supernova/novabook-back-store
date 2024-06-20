1c3be642a0225e04ada823f8880e644be8df4a52
2-say <dev2say@gmail.com>
2024-06-20 23:38:41 +0900

    #45 feat: 공통 응답 문자열일 경우 StringHttpMessageConverter 방지

--- src/main/java/store/novabook/store/common/handler/ApiResponse.java
+++ src/main/java/store/novabook/store/common/handler/ApiResponse.java
@@ -3,6 +3,11 @@ package store.novabook.store.common.handler;
 import java.util.HashMap;
 import java.util.Map;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
 public class ApiResponse<T> {
 
 	private Map<String, Object> header = new HashMap<>();
@@ -14,23 +19,11 @@ public class ApiResponse<T> {
 		this.body = body;
 	}
 
-	public Map<String, Object> getHeader() {
-		return header;
-	}
-
-	public T getBody() {
-		return body;
-	}
-
 	public static <T> ApiResponse<T> success(T body) {
 		return new ApiResponse<>("SUCCESS", true, body);
 	}
 
 	public void addHeader(String key, Object value) {
 		this.header.put(key, value);
-	}
-
-	public void setHeader(Map<String, Object> header) {
-		this.header = header;
 	}
 }
--- src/main/java/store/novabook/store/common/handler/ResponseAdvice.java
+++ src/main/java/store/novabook/store/common/handler/ResponseAdvice.java
@@ -8,6 +8,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.http.MediaType;
 import org.springframework.http.ProblemDetail;
 import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -23,7 +24,7 @@ public class ResponseAdvice implements ResponseBodyAdvice<Object> {
 
 	@Override
 	public boolean supports(MethodParameter returnType, Class<? extends HttpMessageConverter<?>> converterType) {
-		return true;
+		return MappingJackson2HttpMessageConverter.class.isAssignableFrom(converterType);
 	}
 
 	@Override
