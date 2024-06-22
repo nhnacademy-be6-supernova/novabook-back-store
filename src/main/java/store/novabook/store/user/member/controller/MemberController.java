3e4fa660f600d8ecdc6a077ed27f3488d309169f
2-say <dev2say@gmail.com>
2024-06-22 23:45:27 +0900

    #24 feat: view ajax 사용을 위해 checkDuplicated 메서드 Cors 허용

--- src/main/java/store/novabook/store/user/member/controller/MemberController.java
+++ src/main/java/store/novabook/store/user/member/controller/MemberController.java
@@ -7,6 +7,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -38,6 +39,8 @@ public class MemberController {
 		return ResponseEntity.status(HttpStatus.CREATED).body(saved);
 	}
 
+
+	@CrossOrigin(origins = "http://localhost:8080")
 	@GetMapping("/checkDuplicated")
 	public ResponseEntity<Map<String, Boolean>> checkDuplicated(@RequestParam String loginId) {
 		boolean isDuplicateLoginId = memberService.isDuplicateLoginId(loginId);
