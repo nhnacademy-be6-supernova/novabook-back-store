cf6876730dbd5f744e157a93691d8f6481b1c361
munyeongkim2 <wgansdud2@naver.com>
2024-06-23 14:22:07 +0900

    #113 feat : 로그인 구현

--- src/main/java/store/novabook/store/user/member/controller/MemberController.java
+++ src/main/java/store/novabook/store/user/member/controller/MemberController.java
@@ -17,6 +17,8 @@ import lombok.RequiredArgsConstructor;
 import store.novabook.store.user.member.dto.CreateMemberRequest;
 import store.novabook.store.user.member.dto.CreateMemberResponse;
 import store.novabook.store.user.member.dto.GetMemberResponse;
+import store.novabook.store.user.member.dto.LoginMemberRequest;
+import store.novabook.store.user.member.dto.LoginMemberResponse;
 import store.novabook.store.user.member.dto.UpdateMemberRequest;
 import store.novabook.store.user.member.service.MemberService;
 
@@ -43,6 +45,10 @@ public class MemberController {
 	public ResponseEntity<GetMemberResponse> getMember(@PathVariable Long memberId) {
 		GetMemberResponse memberResponse = memberService.getMember(memberId);
 		return ResponseEntity.ok(memberResponse);
+	}
+	@PostMapping("/login")
+	public ResponseEntity<LoginMemberResponse> login(@RequestBody LoginMemberRequest loginMemberRequest){
+		return ResponseEntity.ok().body(memberService.matches(loginMemberRequest));
 	}
 
 	@PutMapping("/{memberId}")
---
+++ src/main/java/store/novabook/store/user/member/dto/LoginMemberRequest.java
@@ -0,0 +1,4 @@
+package store.novabook.store.user.member.dto;
+
+public record LoginMemberRequest (String loginId, String loginPassword){
+}
