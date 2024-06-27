467e0a8c0aa19f98cf06825b6196eb2545b5a45b
ganghagyeong <gkrrod714@naver.com>
2024-06-27 16:10:38 +0900

    #137 feat: dto validation

--- src/main/java/store/novabook/store/user/member/dto/CreateMemberRequest.java
+++ src/main/java/store/novabook/store/user/member/dto/CreateMemberRequest.java
@@ -1,6 +1,7 @@
 package store.novabook.store.user.member.dto;
 
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Builder;
@@ -8,23 +9,23 @@ import lombok.Builder;
 @Builder
 public record CreateMemberRequest(
 
-	@NotNull(message = "아이디는 필수 입력 값입니다.")
+	@NotBlank(message = "아이디는 필수 입력 값입니다.")
 	@Size(max = 20, message = "최대 20자까지 가능합니다.")
 	String loginId,
 
-	@NotNull(message = "비밀번호는 필수 입력 값입니다.")
-	@Size(max = 150, message = "최대 50자까지 가능합니다.")
+	@NotBlank(message = "비밀번호는 필수 입력 값입니다.")
+	@Size(max = 150, message = "최대 150자까지 가능합니다.")
 	String loginPassword,
 
-	@NotNull(message = "이름은 필수 입력 값입니다.")
+	@NotBlank(message = "이름은 필수 입력 값입니다.")
 	@Size(max = 50, message = "최대 50자까지 가능합니다.")
 	String name,
 
-	@NotNull(message = "연락처는 필수 입력 값입니다.")
+	@NotBlank(message = "연락처는 필수 입력 값입니다.")
 	@Size(max = 20, message = "최대 20자까지 가능합니다.")
 	String number,
 
-	@NotNull(message = "이메일은 필수 입력 값입니다.")
+	@NotBlank(message = "이메일은 필수 입력 값입니다.")
 	@Email
 	@Size(max = 100, message = "최대 100자까지 가능합니다.")
 	String email,
