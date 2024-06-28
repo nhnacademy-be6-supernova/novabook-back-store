a1b3f1040f7b8399364e9f1794a1c98b38f36031
2-say <dev2say@gmail.com>
2024-06-28 20:56:05 +0900

    upload setting

--- src/main/java/store/novabook/store/common/config/RabbitConfig.java
+++ src/main/java/store/novabook/store/common/config/RabbitConfig.java
@@ -17,8 +17,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@EnableRabbit
-@Configuration
+// @EnableRabbit
+// @Configuration
 public class RabbitConfig {
 
 	@Value("${spring.rabbitmq.host}")
