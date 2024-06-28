26c0674cfee02410030ab167252e56460a6f5bc0
2-say <dev2say@gmail.com>
2024-06-28 20:27:40 +0900

    upload setting

--- src/main/java/store/novabook/store/StoreApplication.java
+++ src/main/java/store/novabook/store/StoreApplication.java
@@ -2,16 +2,18 @@ package store.novabook.store;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableDiscoveryClient
 @EnableFeignClients
 @EnableJpaAuditing
 @SpringBootApplication
 public class StoreApplication {
 
 	public static void main(String[] args) {
-		System.out.println("젠킨스 12-3012-03091-20491-049-02394-01249-02349-012394-01294-0=1293-012943-01394-1094-01249-02349-02394-23942-30492-304923-40923-049");
+		System.out.println("젠킨스 강하경 ");
 		SpringApplication.run(StoreApplication.class, args);
 	}
 
