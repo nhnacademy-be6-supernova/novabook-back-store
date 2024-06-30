6500ffb50129721ea0a1027c2456c9d0e8d75e74
ganghagyeong <gkrrod714@naver.com>
2024-06-30 18:18:53 +0900

    #184 refactor: pr 피드백 반영

--- src/main/java/store/novabook/store/user/member/dto/CreateMemberRequest.java
+++ src/main/java/store/novabook/store/user/member/dto/CreateMemberRequest.java
@@ -11,7 +11,7 @@ public record CreateMemberRequest(
 	String loginId,
 
 	@NotBlank(message = "비밀번호는 필수 입력 값입니다.")
-	@Size(max = 116, message = "최대 16자까지 가능합니다.")
+	@Size(max = 16, message = "최대 16자까지 가능합니다.")
 	String loginPassword,
 
 	@NotBlank(message = "비밀번호는 필수 입력 값입니다.")
