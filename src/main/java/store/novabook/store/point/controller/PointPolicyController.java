1120dc15896e3a95624ee30afebc69b106b81ab8
hyungwang <kfk10112@gmail.com>
2024-07-02 14:35:16 +0900

    #211 feat: userId aop처

--- src/main/java/store/novabook/store/point/controller/PointPolicyController.java
+++ src/main/java/store/novabook/store/point/controller/PointPolicyController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import store.novabook.store.common.security.aop.CurrentUser;
 import store.novabook.store.point.controller.docs.PointPolicyControllerDocs;
 import store.novabook.store.point.dto.request.CreatePointPolicyRequest;
 import store.novabook.store.point.dto.response.GetPointPolicyResponse;
@@ -26,7 +27,7 @@ public class PointPolicyController implements PointPolicyControllerDocs {
 
 	// @CheckRole("ROLE_USER")
 	@GetMapping
-	public ResponseEntity<Page<GetPointPolicyResponse>> getPoint(Pageable pageable) {
+	public ResponseEntity<Page<GetPointPolicyResponse>> getPoint(@CurrentUser String id, Pageable pageable) {
 		Page<GetPointPolicyResponse> pointPolicyResponseList = pointPolicyService.getPointPolicyList(pageable);
 
 		return ResponseEntity.status(HttpStatus.OK).body(pointPolicyResponseList);
