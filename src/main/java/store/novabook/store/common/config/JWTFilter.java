c02ca6f0b70132e73c23b205270383834d5995b7
hyungwang <kfk10112@gmail.com>
2024-07-02 12:52:00 +0900

    #211 feat: JWTFilter에서 x-user-id확인 후 memberid로 securitycontext에 넣습니다

--- src/main/java/store/novabook/store/common/config/JWTFilter.java
+++ src/main/java/store/novabook/store/common/config/JWTFilter.java
@@ -15,7 +15,6 @@ import lombok.extern.slf4j.Slf4j;
 import reactor.util.annotation.NonNull;
 import store.novabook.store.common.security.dto.CustomUserDetails;
 import store.novabook.store.common.security.entity.Users;
-import store.novabook.store.common.security.service.NewTokenClient;
 import store.novabook.store.member.MemberClient;
 import store.novabook.store.member.dto.request.GetMembersUUIDRequest;
 import store.novabook.store.member.dto.response.GetMembersUUIDResponse;
@@ -23,70 +22,26 @@ import store.novabook.store.member.dto.response.GetMembersUUIDResponse;
 @Slf4j
 public class JWTFilter extends OncePerRequestFilter {
 
-	private final JWTUtil jwtUtil;
 
 	private final MemberClient memberClient;
 
-	private final NewTokenClient newTokenClient;
+	public JWTFilter(MemberClient memberClient) {
 
-	public JWTFilter(JWTUtil jwtUtil, MemberClient memberClient, NewTokenClient newTokenClient) {
-
-		this.jwtUtil = jwtUtil;
 		this.memberClient = memberClient;
-		this.newTokenClient = newTokenClient;
 	}
 
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, @NonNull HttpServletResponse response,
 		@NonNull FilterChain filterChain) throws ServletException, IOException {
 
-		String path = request.getRequestURI();
+		String username = request.getHeader("X-USER-ID");
+		String role = request.getHeader("X-USER-ROLE");
 
-		if (path.startsWith("/api/v1/store/members/find")) {
+		if (username == null || role == null) {
+			log.error("username or role is null");
 			filterChain.doFilter(request, response);
 			return;
 		}
-
-		String authorization = request.getHeader("Authorization");
-		String refresh = request.getHeader("Refresh");
-
-		//Authorization 헤더 검증
-		if (authorization == null || refresh == null) {
-			log.error("token not found");
-			filterChain.doFilter(request, response);
-
-			//조건이 해당되면 메소드 종료 (필수)
-			return;
-		}
-
-		//Bearer 부분 제거 후 순수 토큰만 획득
-		String token = authorization.split(" ")[1];
-
-		//토큰 소멸 시간 검증
-		if (Boolean.TRUE.equals(jwtUtil.isExpired(token))) {
-
-			log.error("token expired");
-
-			// GetNewTokenResponse newToken = newTokenClient.getNewToken(new GetNewTokenRequest(refresh));
-
-			// Cookie expiredCookie = new Cookie("ZZZ", "QQQ");
-			// expiredCookie.setPath("/"); // 경로 설정
-			// expiredCookie.setHttpOnly(false); // HttpOnly 설정 여부
-			// expiredCookie.setSecure(false); // Secure 설정 여부
-			// response.addCookie(expiredCookie);
-
-			// 응답 헤더 설정
-			// response.setHeader("NewToken", newToken.accessToken());
-
-			filterChain.doFilter(request, response);
-
-			//조건이 해당되면 메소드 종료 (필수)
-			return;
-		}
-
-		//토큰에서 username과 role 획득
-		String username = jwtUtil.getUsername(token);
-		String role = jwtUtil.getRole(token);
 
 		GetMembersUUIDRequest getMembersUUIDRequest = new GetMembersUUIDRequest(username);
 
