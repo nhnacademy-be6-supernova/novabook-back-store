47a874f8a2815103f94a156841d30d14bb5dbff2
zaysverse <zeze@jbnu.ac.kr>
2024-07-02 16:37:25 +0900

    fix: JWT 토큰 적용

--- src/main/java/store/novabook/store/member/controller/MemberCouponController.java
+++ src/main/java/store/novabook/store/member/controller/MemberCouponController.java
@@ -4,11 +4,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
+import store.novabook.store.common.security.aop.CurrentUser;
 import store.novabook.store.member.controller.docs.MemberCouponControllerDocs;
 import store.novabook.store.member.dto.request.CreateMemberCouponRequest;
 import store.novabook.store.member.dto.response.CreateMemberCouponResponse;
@@ -21,7 +21,7 @@ public class MemberCouponController implements MemberCouponControllerDocs {
 	private final MemberCouponService memberCouponService;
 
 	@PostMapping
-	public ResponseEntity<CreateMemberCouponResponse> createMemberCoupon(@RequestHeader Long memberId,
+	public ResponseEntity<CreateMemberCouponResponse> createMemberCoupon(@CurrentUser Long memberId,
 		@RequestBody CreateMemberCouponRequest request) {
 		CreateMemberCouponResponse saved = memberCouponService.createMemberCoupon(memberId, request);
 		return ResponseEntity.status(HttpStatus.CREATED).body(saved);
