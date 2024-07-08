2f65d99daaf30afa933bdd992c9435ea757f12b2
ganghagyeong <gkrrod714@naver.com>
2024-07-08 20:55:22 +0900

    #242 refactor: feignClient url yaml 파일로 분리

--- src/main/java/store/novabook/store/member/service/DoorayHookClient.java
+++ src/main/java/store/novabook/store/member/service/DoorayHookClient.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "doorayHookClient", url = "https://hook.dooray.com/services/3204376758577275363/3841573584705463142/XgrMG9YtRw65XfNFuTYFDg")
+@FeignClient(name = "doorayHookClient")
 public interface DoorayHookClient {
 
 	@PostMapping
