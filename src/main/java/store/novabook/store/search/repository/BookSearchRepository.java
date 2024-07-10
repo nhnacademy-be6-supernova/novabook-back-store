500501304b3953249388fdef1685bccb5843225c
munyeongkim2 <wgansdud2@naver.com>
2024-07-10 15:46:10 +0900

    #185 feat : 엘라스틱서치 구현

--- src/main/java/store/novabook/store/search/repository/BookSearchRepository.java
+++ src/main/java/store/novabook/store/search/repository/BookSearchRepository.java
@@ -1,25 +1,32 @@
 package store.novabook.store.search.repository;
 
-import java.util.List;
-
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.elasticsearch.annotations.Query;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
 import store.novabook.store.search.document.BookDocument;
 
 public interface BookSearchRepository extends ElasticsearchRepository<BookDocument, Long> {
-	// 제목에 특정 단어가 포함된 문서 검색
-	List<BookDocument> findAllByTitleContaining(String title);
 
-	// 저자에 특정 단어가 포함된 문서 검색
-	List<BookDocument> findAllByAuthorContaining(String author);
+	@Query("{\"bool\": {\"should\": [" +
+		"{\"match\": {\"title\": \"?0\"}}," +
+		"{\"match\": {\"author\": \"?0\"}}," +
+		"{\"match\": {\"publisher\": \"?0\"}}" +
+		"]}}")
+	Page<BookDocument> findAllByKeywordIgnoreCase(String keyword, Pageable pageable);
 
-	// 제목에 정확한 문구가 포함된 문서 검색
-	@Query("{\"match_phrase\": {\"title\": \"?0\"}}")
-	List<BookDocument> findByTitlePhrase(String title);
+	@Query("{\"bool\": {\"should\": [" +
+		"{\"match\": {\"author\": \"?0\"}}"+
+		"]}}")
+	Page<BookDocument> findAllByAuthorIgnoreCase(String author, Pageable pageable);
 
-	// 저자에 정확한 문구가 포함된 문서 검색
-	@Query("{\"match_phrase\": {\"author\": \"?0\"}}")
-	List<BookDocument> findByAuthorPhrase(String author);
+	@Query("{\"bool\": {\"should\": [" +
+		"{\"match\": {\"publish\": \"?0\"}}"+
+		"]}}")
+	Page<BookDocument> findAllByPublishIgnoreCase(String author, Pageable pageable);
+
+
+	Page<BookDocument> findAllByCategoryListMatches(String category, Pageable pageable);
 
 }
--- src/main/java/store/novabook/store/search/service/impl/BookSearchServiceImpl.java
+++ src/main/java/store/novabook/store/search/service/impl/BookSearchServiceImpl.java
@@ -1,37 +1,60 @@
 package store.novabook.store.search.service.impl;
 
-import java.util.List;
-
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
+import store.novabook.store.common.exception.ErrorCode;
+import store.novabook.store.common.exception.InternalServerException;
 import store.novabook.store.search.document.BookDocument;
+import store.novabook.store.search.dto.GetBookSearchResponse;
 import store.novabook.store.search.repository.BookSearchRepository;
-import store.novabook.store.search.service.BookSearchService;
 
 @Service
 @RequiredArgsConstructor
-public class BookSearchServiceImpl implements BookSearchService {
+public class BookSearchServiceImpl {
 	private final BookSearchRepository bookSearchRepository;
 
-	// 제목에 특정 단어가 포함된 문서 검색
-	public List<BookDocument> searchByTitleContaining(String title) {
-		return bookSearchRepository.findAllByTitleContaining(title);
+	// 모든 단어 검색
+	public Page<GetBookSearchResponse> searchByKeywordContaining(String title, Pageable pageable) {
+		try{
+			Page<BookDocument> bookDocuments = bookSearchRepository.findAllByKeywordIgnoreCase(title, pageable);
+			return bookDocuments.map(GetBookSearchResponse::of);
+		} catch (Exception e){
+			throw new InternalServerException(ErrorCode.INVALID_REQUEST_ARGUMENT);
+		}
 	}
 
 	// 저자에 특정 단어가 포함된 문서 검색
-	public List<BookDocument> searchByAuthorContaining(String author) {
-		return bookSearchRepository.findAllByAuthorContaining(author);
+	public Page<GetBookSearchResponse> searchByAuthorContaining(String author, Pageable pageable) {
+		try{
+			Page<BookDocument> bookDocuments = bookSearchRepository.findAllByAuthorIgnoreCase(author, pageable);
+			return bookDocuments.map(GetBookSearchResponse::of);
+		} catch (Exception e){
+			throw new InternalServerException(ErrorCode.INVALID_REQUEST_ARGUMENT);
+		}
+
 	}
 
-	// 제목에 정확한 문구가 포함된 문서 검색
-	public List<BookDocument> searchByTitlePhrase(String title) {
-		return bookSearchRepository.findByTitlePhrase(title);
+	// 출판사에 특정 단어가 포함된 문서 검색
+	public Page<GetBookSearchResponse> searchByPublishContaining(String author, Pageable pageable) {
+		try{
+			Page<BookDocument> bookDocuments = bookSearchRepository.findAllByPublishIgnoreCase(author, pageable);
+			return bookDocuments.map(GetBookSearchResponse::of);
+		} catch (Exception e){
+			throw new InternalServerException(ErrorCode.INVALID_REQUEST_ARGUMENT);
+		}
 	}
 
-	// 저자에 정확한 문구가 포함된 문서 검색
-	public List<BookDocument> searchByAuthorPhrase(String author) {
-		return bookSearchRepository.findByAuthorPhrase(author);
+	// 카테고리 특정 단어가 포함된 문서 검색
+	public Page<GetBookSearchResponse> searchByCategoryListContaining(String category, Pageable pageable) {
+		try{
+			Page<BookDocument> bookDocuments = bookSearchRepository.findAllByCategoryListMatches(category, pageable);
+			return bookDocuments.map(GetBookSearchResponse::of);
+		} catch (Exception e){
+			throw new InternalServerException(ErrorCode.INVALID_REQUEST_ARGUMENT);
+		}
 	}
 
 
