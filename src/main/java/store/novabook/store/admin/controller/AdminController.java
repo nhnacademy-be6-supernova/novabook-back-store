f9c4fe669dacdfd61f9693b278e2b60b30c838dd
hyungwang <kfk10112@gmail.com>
2024-07-13 15:54:13 +0900

    fix : 권환체크 비활성화

--- src/main/java/store/novabook/store/admin/controller/AdminController.java
+++ src/main/java/store/novabook/store/admin/controller/AdminController.java
@@ -6,14 +6,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
-import store.novabook.store.common.security.aop.CheckRole;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/store/admin")
 public class AdminController {
 
-	@CheckRole("ROLE_ADMIN")
+	// 임시 비활성화(개발편의)
+	// @CheckRole("ROLE_ADMIN")
 	@GetMapping()
 	public ResponseEntity<Void> admin() {
 		return ResponseEntity.ok().build();
