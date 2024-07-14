eba03eb989d4017338b80dd64b4fe3eef874ae88
2-say <dev2say@gmail.com>
2024-07-15 00:24:29 +0900

    #209 fix: 포인트 && 쿠폰 동시 적용이 안되는 문제 해결 if문을 잘못 사용함.

--- src/main/java/store/novabook/store/orders/service/impl/OrdersSagaManagerImpl.java
+++ src/main/java/store/novabook/store/orders/service/impl/OrdersSagaManagerImpl.java
@@ -50,27 +50,22 @@ public class OrdersSagaManagerImpl {
 			boolean usePoint = !orderSagaMessage.isNoUsePoint();
 			boolean useCoupon = !orderSagaMessage.isNoUseCoupon();
 
-			if (usePoint) {
-				orderSagaMessage.setStatus("PROCEED_POINT_DECREMENT");
-				rabbitTemplate.convertAndSend(NOVA_ORDERS_SAGA_EXCHANGE, "point.decrement.routing.key", orderSagaMessage);
-			}
-
-			if (useCoupon) {
-				orderSagaMessage.setStatus("PROCEED_APPLY_COUPON");
-				rabbitTemplate.convertAndSend(NOVA_ORDERS_SAGA_EXCHANGE, "coupon.apply.routing.key", orderSagaMessage);
-			}
-
 			if (!usePoint && !useCoupon) {
 				orderSagaMessage.setStatus("PROCEED_APPROVE_PAYMENT");
 				rabbitTemplate.convertAndSend(NOVA_ORDERS_SAGA_EXCHANGE, "orders.approve.payment.routing.key", orderSagaMessage);
+			} else if (!useCoupon) {
+				orderSagaMessage.setStatus("PROCEED_POINT_DECREMENT");
+				rabbitTemplate.convertAndSend(NOVA_ORDERS_SAGA_EXCHANGE, "point.decrement.routing.key", orderSagaMessage);
+			} else {
+				orderSagaMessage.setStatus("PROCEED_APPLY_COUPON");
+				rabbitTemplate.convertAndSend(NOVA_ORDERS_SAGA_EXCHANGE, "coupon.apply.routing.key", orderSagaMessage);
 			}
 		} else if ("FAIL_CONFIRM_ORDER_FORM".equals(orderSagaMessage.getStatus())) {
 			rabbitTemplate.convertAndSend(NOVA_ORDERS_SAGA_EXCHANGE, "nova.orders.saga.dead.routing.key", orderSagaMessage);
 			log.error("주문서 검증 실패");
-		} else {
-			log.warn("알 수 없는 상태: {}", orderSagaMessage.getStatus());
 		}
 	}
+
 
 
 	/**
