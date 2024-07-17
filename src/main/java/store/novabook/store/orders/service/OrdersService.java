121ecdbd5e2e9805cf2d058afcf78e1b61b0b9ac
2-say <dev2say@gmail.com>
2024-07-17 10:09:21 +0900

    #209 test: 사용하지 않는 import 제거

--- src/main/java/store/novabook/store/orders/service/OrdersService.java
+++ src/main/java/store/novabook/store/orders/service/OrdersService.java
@@ -3,12 +3,9 @@ package store.novabook.store.orders.service;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
-import store.novabook.store.orders.dto.request.CreateOrdersRequest;
 import store.novabook.store.orders.dto.request.UpdateOrdersAdminRequest;
-import store.novabook.store.orders.dto.response.CreateResponse;
 import store.novabook.store.orders.dto.response.GetOrdersAdminResponse;
 import store.novabook.store.orders.dto.response.GetOrdersResponse;
-import store.novabook.store.payment.entity.Payment;
 
 public interface OrdersService {
 
