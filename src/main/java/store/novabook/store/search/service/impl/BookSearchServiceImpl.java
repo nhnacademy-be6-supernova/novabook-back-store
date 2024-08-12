650073d08cd25cb53f1a2074c2c87b4a0565b26c
munyeongkim2 <wgansdud2@naver.com>
2024-08-13 03:05:42 +0900

    refactor : 엘라스틱서치 수정

--- src/main/java/store/novabook/store/search/service/impl/BookSearchServiceImpl.java
+++ src/main/java/store/novabook/store/search/service/impl/BookSearchServiceImpl.java
@@ -1,7 +1,12 @@
 package store.novabook.store.search.service.impl;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.*;
+import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
+import org.springframework.data.elasticsearch.core.SearchHit;
+import org.springframework.data.elasticsearch.core.SearchHits;
+import org.springframework.data.elasticsearch.core.query.Criteria;
+import org.springframework.data.elasticsearch.core.query.CriteriaQuery;
+import org.springframework.data.elasticsearch.core.query.Query;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
@@ -11,16 +16,22 @@ import store.novabook.store.search.document.BookDocument;
 import store.novabook.store.search.dto.GetBookSearchResponse;
 import store.novabook.store.search.repository.BookSearchRepository;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 @RequiredArgsConstructor
 public class BookSearchServiceImpl {
 	private final BookSearchRepository bookSearchRepository;
+	private final ElasticsearchOperations elasticsearchOperations;
 
 	// 모든 단어 검색
 	public Page<GetBookSearchResponse> searchByKeywordContaining(String keyword, Pageable pageable) {
 		try{
 			Page<BookDocument> bookDocuments = bookSearchRepository.findAll(keyword, pageable);
 			return bookDocuments.map(GetBookSearchResponse::of);
+
 		} catch (Exception e){
 			throw new InternalServerException(ErrorCode.INVALID_REQUEST_ARGUMENT);
 		}
@@ -34,7 +45,6 @@ public class BookSearchServiceImpl {
 		} catch (Exception e){
 			throw new InternalServerException(ErrorCode.INVALID_REQUEST_ARGUMENT);
 		}
-
 	}
 
 	// 출판사에 특정 단어가 포함된 문서 검색
